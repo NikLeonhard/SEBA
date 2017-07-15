7bcbf1b93ae01f3b0a6bd3441f145549dc3e35ab
NIK-PC\Niklas <nikleonhard@gmail.com>
2017-07-15 13:54:37 +0200

    -finished search function -added content to legalAdvice and Tutorial -fixed some redirect bugs

--- Frontend/src/components/view-listings-sse/view-listings-sse.component.js
+++ Frontend/src/components/view-listings-sse/view-listings-sse.component.js
@@ -39,6 +39,49 @@ class ViewListingsSseComponentController{
     static get $inject(){
         return ['$state', ListingsService.name, UserService.name];
     }
+
+    queryTitle(input, listing){
+        if(typeof input == 'undefined' || input.toString().length==0)
+            return true;
+
+        return(listing.title.includes(input.toString()));
+    }
+
+
+    queryRange(lowerEnd,upperEnd,listing){
+        var lowerEndInt = parseInt(lowerEnd,10);
+        var upperEndInt = parseInt(upperEnd,10);
+
+
+        if(lowerEndInt.toString()=="NaN" && upperEndInt.toString()=="NaN") {
+            return true
+        }
+
+
+
+        if(lowerEndInt==0 && upperEndInt==0) {
+            return true;
+        }
+
+        if(lowerEndInt>upperEndInt)
+            return (listing.amount>=upperEndInt && listing.amount<=lowerEndInt)
+
+
+        else {
+            return (listing.amount<=upperEndInt && listing.amount>=lowerEndInt)
+        }
+    }
+
+    queryZipCode(zipCode, listing) {
+        if(typeof zipCode === 'undefined' || zipCode.toString().length ==0)
+            return true;
+
+        return listing.postcode.includes(zipCode);
+    }
+
+    queryAll(input,lowerEnd,upperEnd,listing,zipCode){
+        return (this.queryTitle(input,listing) && this.queryRange(lowerEnd,upperEnd,listing)&& this.queryZipCode(zipCode,listing));
+    }
 }
 
 export default ViewListingsSseComponent;
