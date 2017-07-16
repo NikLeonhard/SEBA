769b1099f939cd95078eb5b46e236b92d7b43f3a
Katsulynx <katsulynx@gmail.com>
2017-07-16 13:05:15 +0200

    Messages Backend extended

--- Backend/message/messageController.js
+++ Backend/message/messageController.js
@@ -4,6 +4,8 @@ var Message = require('./messageSchema');
 // TODO: Get conversation list of user
 // Create endpoint /api/messages for GET
 exports.getMessageList = function(req, res) {
+	// TODO: GET ALL CONVERSATIONS FOR CURRENT USER
+
 	/*
     Message.find(function(err, messages) {
         if (err) {
@@ -18,6 +20,8 @@ exports.getMessageList = function(req, res) {
 // TODO: Get all messages of a conversation
 // Create endpoint /api/messages/:message_id for GET
 exports.getMessages = function(req, res) {
+	// TODO: GET MESSAGES EXCHANGED WITH CONVERSATION PARTNER USERID (= CONVERSATION ID)
+
 	/*
     Message.findById(req.params.message_id, function(err, message) {
         if (err) {
@@ -33,21 +37,21 @@ exports.getMessages = function(req, res) {
 // TODO: Save a new Message to a conversation
 // Create endpoint /api/messages/:message_id for PUT
 exports.putMessage = function(req, res) {
-	/*
-    Message.findByIdAndUpdate(
-        req.params.message_id,
-        req.body,
-        {
-            //pass the new object to cb function
-            new: true,
-            //run validations
-            runValidators: true
-        }, function (err, message) {
-            if (err) {
-                res.status(400).send(err);
-                return;
-            }
-            res.json(message);
-        });
-	*/
+	// TODO: INSERT MESSAGE INTO MESSAGES
+
+	Message.findByIdAndUpdate(
+		req.params.message_id,
+		req.body,
+		{
+			//pass the new object to cb function
+			new: true,
+			//run validations
+			runValidators: true
+		}, function (err, message) {
+			if (err) {
+				res.status(400).send(err);
+				return;
+			}
+			res.json(message);
+		});
 };
